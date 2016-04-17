5b8d7ea457c1673df60af940c064f848b2e4b822
James Newman <jamesmnewman1@gmail.com>
2016-04-17 15:56:34 -0700

    uncomment next 2

--- Ruby/spec/algorithms_spec.rb
+++ Ruby/spec/algorithms_spec.rb
@@ -12,20 +12,20 @@ describe 'caesar_cipher' do
   specify { expect(caesar_cipher('asdf asdf', 13)).to eq('nfqs nfqs') }
 end
 
-# describe 'common_substrings' do
-#   specify { expect(common_substrings('Hello', 'Hello World')).to eq('Hello') }
-#   specify { expect(common_substrings('ABABC', 'BABCA')).to eq('BABC') }
-# end
+describe 'common_substrings' do
+  specify { expect(common_substrings('Hello', 'Hello World')).to eq('Hello') }
+  specify { expect(common_substrings('ABABC', 'BABCA')).to eq('BABC') }
+end
 
-# describe 'sum_rec' do
-#   specify { expect(sum_rec([1, 2, 3])).to eq(6) }
-#   specify { expect(sum_rec([99, 66, 33])).to eq(198) }
+describe 'sum_rec' do
+  specify { expect(sum_rec([1, 2, 3])).to eq(6) }
+  specify { expect(sum_rec([99, 66, 33])).to eq(198) }
 
-#   it 'calls itself' do
-#     expect(self).to receive(:sum_rec).at_least(:twice).and_call_original
-#     sum_rec([10, 20, 30])
-#   end
-# end
+  it 'calls itself' do
+    expect(self).to receive(:sum_rec).at_least(:twice).and_call_original
+    sum_rec([10, 20, 30])
+  end
+end
 
 # describe 'fibs' do
 #   specify { expect(fibs(3)).to eq([0, 1, 1]) }
