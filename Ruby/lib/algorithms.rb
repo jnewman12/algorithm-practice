373121ccd7282e3bfb50d104f9945264827c26b5
James Newman <jamesmnewman1@gmail.com>
2016-05-12 23:54:58 -0700

    might as well do that one next

--- Ruby/lib/algorithms.rb
+++ Ruby/lib/algorithms.rb
@@ -90,7 +90,7 @@ end
 # It should return true if the input is a valid IPv4 address.
 # Valid IPs are anything between '0.0.0.0' and '255.255.255.255'.
 def valid_ip?(string)
-
+  # next
 end
 
 # Implement the Folding Cipher.
