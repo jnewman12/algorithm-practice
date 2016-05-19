c51b74f7bc85f861df417613daa6b8d96e1dad25
James Newman <jamesmnewman1@gmail.com>
2016-05-19 00:30:37 -0700

    space

--- Ruby/lib/algorithms.rb
+++ Ruby/lib/algorithms.rb
@@ -120,9 +120,6 @@ end
 # Write a function that takes a year as a four digit integer.
 # Returns an array of the 10 closest subsequent silly years.
 # A silly year's first two digits plus the last two digits equal the middle two.
-# def silly_years(year)
-
-# end
 def silly_years(year)
   result = []
   while result.length < 10
