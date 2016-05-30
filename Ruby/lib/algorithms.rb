7719de7f908ca68db23c886729d5ea98236ac070
James Newman <jamesmnewman1@gmail.com>
2016-05-30 00:33:38 -0700

    missed

--- Ruby/lib/algorithms.rb
+++ Ruby/lib/algorithms.rb
@@ -146,7 +146,6 @@ end
 # Find the sum of numbers falling inside the rectangle.
 # Time complexity: O(number of rows * number of columns).
 def matrix_region_sum(matrix, top_left_coords, bottom_right_coords)
-
 end
 
 # Implement Merge Sort
