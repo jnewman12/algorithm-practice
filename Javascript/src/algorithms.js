fff3edcba741458484a9fd53a647d50a62d2c4a5
James Newman <jamesmnewman1@gmail.com>
2016-05-31 21:54:26 -0700

    ..

--- Javascript/src/algorithms.js
+++ Javascript/src/algorithms.js
@@ -97,7 +97,7 @@ Algorithms.foldingCipher = function (string) {
 
 // Write a method that finds all the unique substrings for a word.
 Algorithms.uniqSubs = function (string) {
-
+  // really?
 };
 
 
